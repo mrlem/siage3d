e3b00dd3ed3ce60b68ff2f86450d7e0758881d2b
Sebastien Guillemin <seb@mrlem.org>
2019-11-05 08:48:45 +0100

    scenegraph: add box shape

--- k3d-core/src/main/java/org/mrlem/k3d/core/scene/shapes/Box.kt
+++ k3d-core/src/main/java/org/mrlem/k3d/core/scene/shapes/Box.kt
@@ -1,5 +1,135 @@
 package org.mrlem.k3d.core.scene.shapes
 
-// TODO - major - implement
+class Box : Shape(
+    positions,
+    texCoords,
+    indices,
+    normals
+) {
 
-class Box(width: Float, height: Float, depth: Float) : Shape(floatArrayOf(), floatArrayOf(), shortArrayOf(), floatArrayOf())
+    companion object {
+
+        private val positions = floatArrayOf(       // counterclockwise:
+
+            // front
+            -0.5f,  0.5f, 0.5f,                     // top left
+            -0.5f, -0.5f, 0.5f,                     // bottom left
+             0.5f, -0.5f, 0.5f,                     // bottom right
+             0.5f,  0.5f, 0.5f,                     // top right
+            // back
+             0.5f,  0.5f, -0.5f,
+             0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f,  0.5f, -0.5f,
+            // top
+            -0.5f, 0.5f, -0.5f,
+            -0.5f, 0.5f,  0.5f,
+             0.5f, 0.5f,  0.5f,
+             0.5f, 0.5f, -0.5f,
+            // bottom
+            -0.5f, -0.5f,  0.5f,
+            -0.5f, -0.5f, -0.5f,
+             0.5f, -0.5f, -0.5f,
+             0.5f, -0.5f,  0.5f,
+            // left
+            -0.5f,  0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+            // right
+            0.5f,  0.5f,  0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f
+        )
+
+        private val texCoords = floatArrayOf(
+            // front
+            0f, 1f,
+            0f, 0f,
+            1f, 0f,
+            1f, 1f,
+            // back
+            0f, 1f,
+            0f, 0f,
+            1f, 0f,
+            1f, 1f,
+            // top
+            0f, 1f,
+            0f, 0f,
+            1f, 0f,
+            1f, 1f,
+            // bottom
+            0f, 1f,
+            0f, 0f,
+            1f, 0f,
+            1f, 1f,
+            // left
+            0f, 1f,
+            0f, 0f,
+            1f, 0f,
+            1f, 1f,
+            // right
+            0f, 1f,
+            0f, 0f,
+            1f, 0f,
+            1f, 1f
+        )
+
+        private val indices = shortArrayOf(
+            // front
+            0, 1, 2,
+            0, 2, 3,
+            // back
+            4, 5, 6,
+            4, 6, 7,
+            // top
+            8, 9, 10,
+            8, 10, 11,
+            // bottom
+            12, 13, 14,
+            12, 14, 15,
+            // left
+            16, 17, 18,
+            16, 18, 19,
+            // right
+            20, 21, 22,
+            20, 22, 23
+        )
+
+        private val normals = floatArrayOf(
+            // front
+            0f, 0f, 1f,
+            0f, 0f, 1f,
+            0f, 0f, 1f,
+            0f, 0f, 1f,
+            // back
+            0f, 0f, -1f,
+            0f, 0f, -1f,
+            0f, 0f, -1f,
+            0f, 0f, -1f,
+            // top
+            0f, 1f, 0f,
+            0f, 1f, 0f,
+            0f, 1f, 0f,
+            0f, 1f, 0f,
+            // bottom
+            0f, -1f, 0f,
+            0f, -1f, 0f,
+            0f, -1f, 0f,
+            0f, -1f, 0f,
+            // left
+            -1f, 0f, 0f,
+            -1f, 0f, 0f,
+            -1f, 0f, 0f,
+            -1f, 0f, 0f,
+            // right
+            1f, 0f, 0f,
+            1f, 0f, 0f,
+            1f, 0f, 0f,
+            1f, 0f, 0f
+        )
+
+    }
+
+}
--- k3d-sample/src/main/java/org/mrlem/k3d/sample/SceneAdapter.kt
+++ k3d-sample/src/main/java/org/mrlem/k3d/sample/SceneAdapter.kt
@@ -7,9 +7,13 @@ import org.mrlem.k3d.core.common.io.TextureCache
 import org.mrlem.k3d.core.scene.ObjectNode
 import org.mrlem.k3d.core.scene.Scene
 import org.mrlem.k3d.core.scene.materials.TextureMaterial
-import org.mrlem.k3d.core.scene.shapes.Square
+import org.mrlem.k3d.core.scene.shapes.Box
 import kotlin.math.sin
 
+// TODO - major - onstop + onstart (app-list, then select app) causes dup'ed objects to appear
+// TODO - medium - camera fps controller
+// TODO - medium - skybox
+// TODO - minor - shader should be attached to material
 // TODO - optional - kotlin dsl for scene / subgraph init
 
 class SceneAdapter(
@@ -22,18 +26,18 @@ class SceneAdapter(
 
     override fun onInit() {
         // TODO - major - fails with obj loader shapes: why?
-        val square = Square() //Shape(resources, R.raw.model_tree_lowpoly_mesh)
+        val cube = Box() //Shape(resources, R.raw.model_tree_lowpoly_mesh)
         val material = TextureMaterial(TextureCache.get(resources, R.raw.model_tree_lowpoly_texture))
 
         scene.apply {
             camera.position(Vector3f(0f, 0f, 1f))
             skyColor.set(.6f, .8f, 1f)
             add(
-                ObjectNode(square, material),
-                ObjectNode(square, material).apply { position(Vector3f(1f, 1f, 0f)) },
-                ObjectNode(square, material).apply { position(Vector3f(1f, -1f, 0f)) },
-                ObjectNode(square, material).apply { position(Vector3f(-1f, -1f, 0f)) },
-                ObjectNode(square, material).apply { position(Vector3f(-1f, 1f, 0f)) }
+                ObjectNode(cube, material),
+                ObjectNode(cube, material).apply { position(Vector3f(1f, 1f, 0f)) },
+                ObjectNode(cube, material).apply { position(Vector3f(1f, -1f, 0f)) },
+                ObjectNode(cube, material).apply { position(Vector3f(-1f, -1f, 0f)) },
+                ObjectNode(cube, material).apply { position(Vector3f(-1f, 1f, 0f)) }
             )
         }
     }
@@ -42,12 +46,12 @@ class SceneAdapter(
         time += delta
 
         // animate camera
-        val value = sin(time * 4) * 0.5f + .5f
+        val value = sin(time * 2) * 0.5f + .5f
         scene.camera.pitch = value * 5
         scene.camera.position.z = value * 10 + 1f
 
         // animate the scene
-        scene.rotation(Vector3f(0f, 0f, value))
+        scene.rotation(Vector3f(0f, value * 6, 0f))
         scene.children[1].rotation(Vector3f(0f, 0f, value))
         scene.children[2].rotation(Vector3f(0f, 0f, value))
         scene.children[3].rotation(Vector3f(0f, 0f, value))
