34fae6d17547f4b63fe1d859144d6730a9f594bc
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2020-04-02 14:29:46 +0200

    TMP - make it easier to debug shadows

--- sample/src/main/java/org/mrlem/siage3d/sample/SimpleSceneAdapter.kt
+++ sample/src/main/java/org/mrlem/siage3d/sample/SimpleSceneAdapter.kt
@@ -2,8 +2,11 @@ package org.mrlem.siage3d.sample
 
 import org.mrlem.siage3d.core.scene.ObjectNode
 import org.mrlem.siage3d.core.scene.dsl.*
+import org.mrlem.siage3d.core.scene.lights.DirectionLight
+import org.mrlem.siage3d.core.scene.materials.TextureMaterial
 import org.mrlem.siage3d.core.scene.rotate
 import org.mrlem.siage3d.core.scene.shapes.Box
+import org.mrlem.siage3d.core.scene.shapes.Square
 import org.mrlem.siage3d.core.view.SceneAdapter
 
 /**
@@ -12,6 +15,7 @@ import org.mrlem.siage3d.core.view.SceneAdapter
 class SimpleSceneAdapter : SceneAdapter() {
 
     private val cube by lazy { scene.get<ObjectNode>("my-cube")!! }
+    private val sun by lazy { scene.get<DirectionLight>("sun")!! }
     private var time = 0f
 
     override fun onSceneCreate() = scene {
@@ -32,6 +36,22 @@ class SimpleSceneAdapter : SceneAdapter() {
             material { texture(R.drawable.crate1_diffuse) }
             position(0f, 1f, -2f)
         }
+
+        material("shadowMap") {}
+
+        objectNode("preview", Square()) {
+            material("shadowMap")
+            position(-.3f, 2.7f, 2f)
+            scale(0.5f)
+        }
+    }
+
+    override fun onSceneCreated() {
+        // preview shadow-map
+        scene.materials
+            .filterIsInstance(TextureMaterial::class.java)
+            .first { it.name == "shadowMap" }
+            .apply { texture = sun.shadowMap.texture }
     }
 
     override fun onUpdate(delta: Float) {
