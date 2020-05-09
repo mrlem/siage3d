36682ed0da40a888fb792755fbcb9624a62dcf97
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2020-05-09 14:12:48 +0200

    api: simplify non-scene activity integration

--- core/src/main/java/org/mrlem/siage3d/core/SceneActivity.kt
+++ core/src/main/java/org/mrlem/siage3d/core/SceneActivity.kt
@@ -5,20 +5,20 @@ import android.view.View
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_scene.*
 import org.mrlem.siage3d.core.view.SceneAdapter
 import org.mrlem.siage3d.core.view.SceneView
 
 abstract class SceneActivity : AppCompatActivity() {
 
-    @LayoutRes open val layoutId: Int = R.layout.activity_main
+    @LayoutRes open val layoutId: Int = R.layout.activity_scene
     @IdRes open val sceneId: Int = R.id.sceneView
 
     abstract val sceneAdapter: SceneAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initFullscreen()
+        setupFullscreen()
         setContentView(layoutId)
         findViewById<SceneView>(sceneId).adapter = sceneAdapter
     }
@@ -33,12 +33,12 @@ abstract class SceneActivity : AppCompatActivity() {
         super.onStop()
     }
 
-    private fun initFullscreen() {
-        val uiVisibility = window.decorView.systemUiVisibility
-        window.decorView.systemUiVisibility = uiVisibility or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-    }
+}
 
+fun AppCompatActivity.setupFullscreen() {
+    val uiVisibility = window.decorView.systemUiVisibility
+    window.decorView.systemUiVisibility = uiVisibility or
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 }
