6b8ac5e57a4dd3be97ac4d8977e896c7bf198d16
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2020-05-21 10:31:03 +0200

    doc: add kdoc for scene

--- core/src/main/java/org/mrlem/siage3d/core/scene/graph/Scene.kt
+++ core/src/main/java/org/mrlem/siage3d/core/scene/graph/Scene.kt
@@ -7,11 +7,20 @@ import org.mrlem.siage3d.core.scene.graph.nodes.lights.LightNode
 import org.mrlem.siage3d.core.scene.graph.nodes.skies.ColorSkyNode
 import org.mrlem.siage3d.core.scene.graph.nodes.skies.SkyNode
 
+/**
+ * The 3D scene to be rendered on screen. It consists in a graph of interconnected nodes describing the objects to draw
+ * and their positions. The Scene is effectively this graph's root node.
+ *
+ * @property name the name used to identify this scene.
+ */
 class Scene(name: String?) : GroupNode(name) {
 
     val materials: MutableList<Material> = mutableListOf()
 
-    // nodes accessors
+    ///////////////////////////////////////////////////////////////////////////
+    // Node accessors, for convenience.
+    ///////////////////////////////////////////////////////////////////////////
+
     val camera: CameraNode get() = children.filterIsInstance(CameraNode::class.java).firstOrNull() ?: CameraNode("camera")
     val sky: SkyNode get() = children.filterIsInstance(SkyNode::class.java).firstOrNull() ?: ColorSkyNode()
     val lights get() = children.filterIsInstance(LightNode::class.java)
